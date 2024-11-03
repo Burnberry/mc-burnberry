947b79509050eda716cacd626f7aaa8d9725424b
Noppe <CoinCabal@outlook.com>
2024-11-03 08:45:16 +0100

    Start

---
+++ src/main/java/noppe/minecraft/burnberry/Burnberry.java
@@ -0,0 +1,17 @@
+package noppe.minecraft.burnberry;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Burnberry extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
